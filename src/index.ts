6c257735a253ac8941c8572b21006e5a9379a384
LuisMayo <luismayovalbuena@outlook.es>
2020-06-25 18:09:56 +0200

    Changed port

--- src/index.ts
+++ src/index.ts
@@ -68,7 +68,7 @@ app.get('/url/:url', function (req, res) {
   downloadAndProcessVideo(req.params.url, res);
 });
 
-app.listen(3000, function () {
+app.listen(80, function () {
   console.log('Example app listening on port 3000!');
 });
 
